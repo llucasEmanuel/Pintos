43e1ec1ecb3b03862576900d9644b732ef82c2fa
Lucas Emanuel <131834616+llucasEmanuel@users.noreply.github.com>
2024-06-23 15:13:40 -0300

    Update thread.h

--- threads/thread.h
+++ threads/thread.h
@@ -24,6 +24,9 @@ typedef int tid_t;
 #define PRI_DEFAULT 31                  /* Default priority. */
 #define PRI_MAX 63                      /* Highest priority. */
 
+
+// Lista das threads bloqueadas
+extern struct list sleep_list;
 /* A kernel thread or user process.
 
    Each thread structure is stored in its own 4 kB page.  The
